b41d47f698ad9e58e8a1293b080560477721ec36
sankarshan <ssankarshan636@gmail.com>
2025-06-14 16:11:27 +0530

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: `http://localhost:5173`,
+    origin: `https://po-p-x-frontend-7pqt.vercel.app/`,
     credentials: true
 }))
 console.log(process.env.CORS_ORIGIN);
